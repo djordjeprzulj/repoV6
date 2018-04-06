cad5b2dba905ef361fa716d2d10013e0b1ed68a4
djordje <przulj@gmail.com>
2018-04-06 22:33:12 +0200

    Dodata metoda u servis porudžbine za dodavanje porudžbine sa stavkama kao jedna transakcija.

--- V5Data/src/main/java/com/example/v5data/service/TOrderService.java
+++ V5Data/src/main/java/com/example/v5data/service/TOrderService.java
@@ -1,8 +1,28 @@
 package com.example.v5data.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.v5data.model.TOrder;
+import com.example.v5data.model.TOrderItem;
+import com.example.v5data.repository.TOrderItemRepository;
+import com.example.v5data.repository.TOrderRepository;
 
 @Service
 public class TOrderService {
 
+	@Autowired
+	TOrderRepository tOrderRepository;
+	
+	@Autowired
+	TOrderItemRepository tOrderItemRepository;
+	
+	
+	@Transactional
+	public void insertOrderAndItems(TOrder order) {
+		tOrderRepository.save(order);
+		for (TOrderItem item : order.getTOrderItems())
+			tOrderItemRepository.save(item);
+	}
 }
