e79f3029af6e6e737a2c8807ffb68afbce9eb3eb
djordje <przulj@gmail.com>
2018-04-06 20:51:14 +0200

    Inicijalni komit.

---
+++ V5Data/src/main/java/com/example/v5data/V5DataApplication.java
@@ -0,0 +1,12 @@
+package com.example.v5data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V5DataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V5DataApplication.class, args);
+	}
+}
